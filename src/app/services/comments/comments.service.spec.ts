b51d73c1f29df2b5bcf03d8b87dae8d99c61563a
Cristina Zlatov <zlatovcristina@gmail.com>
2023-08-14 00:48:48 +0200

    comments service complete testing

--- src/app/services/comments/comments.service.spec.ts
+++ src/app/services/comments/comments.service.spec.ts
@@ -6,8 +6,8 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { Comment } from './comments';
 
 describe('CommentsService', () => {
-  let service: CommentsService;
-  let httpMock: HttpTestingController;
+  let commentsService: CommentsService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,12 +15,44 @@ describe('CommentsService', () => {
       providers: [AuthService, CommentsService],
     });
 
-    service = TestBed.inject(CommentsService);
-    httpMock = TestBed.inject(HttpTestingController);
+    commentsService = TestBed.inject(CommentsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
 
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(commentsService).toBeTruthy();
   });
+
+  it('should get post comments', () => {
+    const postId = 1;
+    const mockComments: Comment[] = [
+      {id: 1, post_id: 1, name: 'name1', email: 'email1', body: 'string1',},
+      {id: 2, post_id: 1, name: 'name2', email: 'email2', body: 'string2',},
+    ];
+
+    commentsService.getPostComments(postId)
+    .subscribe((comments: Comment[]) => {
+      expect(comments).toEqual(mockComments)
+    })
+
+    const req = httpTestingController.expectOne('https://gorest.co.in/public/v2/comments');
+    expect(req.request.method).toEqual('GET')
+    req.flush(mockComments);
+  })
+
+  it('should add a comment', () => {
+    const newComment: Comment = 
+    {id: 1, post_id: 1, name: 'name1', email: 'email1', body: 'string1'}
+
+    commentsService.addComment(newComment)
+    .subscribe(response => {
+      expect(response).toEqual(newComment);
+    })
+
+    const req = httpTestingController.expectOne('https://gorest.co.in/public/v2/comments')
+    expect(req.request.method).toEqual('POST')
+    req.flush(newComment)
+  })
+
 });
