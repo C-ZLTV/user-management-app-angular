9aba2ea1e5c2a71f00f0969ffc128227440b8917
Cristina Zlatov <zlatovcristina@gmail.com>
2023-08-26 17:44:11 +0200

    404 style

--- src/app/components/four0four/four0four.component.ts
+++ src/app/components/four0four/four0four.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-four0four',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class Four0fourComponent {
 
+  constructor(private location: Location){}
+
+  goBack(){
+    this.location.back();
+  }
 }
